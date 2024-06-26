2c11f35bc9b49fc71fea0d8940bdc2762e92b82e
DaviSouzaOlie <dribeiro@olie.ai>
2024-06-25 21:40:08 -0300

    more primitive types and some functions

--- modulo-02/2.1/typeAnnotation.ts
+++ modulo-02/2.1/typeAnnotation.ts
@@ -25,3 +25,38 @@ console.log(multNumber(12, 12));
 let bool : boolean = 12 > 20;
 
 console.log(bool);
+
+
+let nums: Array<number> = [1, 2, 3, 4, 5];
+
+let moreNums: Array<number> = [...nums, 6, 7, 8, 9, 10]
+
+console.log(moreNums);
+
+// Tuple
+
+let tuple : [string, number, boolean];
+
+tuple = ['Davi', 12, true];
+// tuple = ['Davi', true, 12]; -- NAO PODE
+
+console.log(tuple);
+
+let listFruits : [string, ...string[]] = ['apple', 'orange', 'banana', 'lemon']
+console.log(...listFruits);
+
+let listFruits2 : [number, boolean, ...string[]] = [5, true, ...listFruits]
+console.log(...listFruits2);
+
+// Functions:
+
+type Name =
+        | [firstName: string, string: string]
+        | [firstName: string, middleName: string, lastName: string]
+
+function createPeople(...name: Name) {
+    return [...name];
+}
+
+console.log(createPeople('Davi', 'Souza'))
+console.log(createPeople('Davi', 'Ribeiro', 'Souza'))
