eaedce2be23478360e3271f349b36ba5b9d77540
DaviSouzaOlie <dribeiro@olie.ai>
2024-06-28 22:04:09 -0300

    class

--- class.ts
+++ class.ts
@@ -1,9 +1,13 @@
 class car {
 
-    constructor(make: string, color: string, doors: number) {
+    constructor(make: string, color: string, doors = 4) {
         this._make = make;
         this._color = color;
-        this._doors = doors;
+        if ((doors % 2) === 0) {
+            this._doors = doors;
+        } else {
+            throw new Error('Doors must be an even number');
+        }
     }
 
     _make: string;
@@ -33,3 +37,45 @@ class car {
         }
     }
 }
+
+let myCar2 = new car('Galaxy Motors', 'red', 3);
+
+let myCar3 = new car('Galaxy Motors', 'gray');
+console.log(myCar3.doors);  // returns 4, the default value
+
+console.log(myCar2.accelerate(35));
+console.log(myCar2.brake());
+console.log(myCar2.turn('right'));
+
+// Properties
+private _make: string;
+private _color: string;
+private _doors: number;
+// ...
+private worker(): string {
+    return this._make;
+}
+
+class Car {
+    // Properties
+    private static numberOfCars: number = 0;  // New static property
+    private _make: string;
+    private _color: string;
+    private _doors: number;
+
+    // Constructor
+    constructor(make: string, color: string, doors = 4) {
+        this._make = make;
+        this._color = color;
+        this._doors = doors;
+        Car.numberOfCars++; // Increments the value of the static property
+    }
+    // ...
+}
+
+// Instantiate the Car object with all parameters
+let myCar1 = new Car('Cool Car Company', 'blue', 2);
+// Instantiates the Car object with all parameters
+let myCar2 = new Car('Galaxy Motors', 'blue', 2);
+// Returns 2
+console.log(Car.getNumberOfCars());
